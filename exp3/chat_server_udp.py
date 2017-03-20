b899ff5aea7870982f2c12b2e26ce74fb2b444a3
Sekhar Karedla <sekhar.karedla@gmail.com>
2017-03-20 18:46:12 +0530

    Update chat_server_udp.py

--- exp3/chat_server_udp.py
+++ exp3/chat_server_udp.py
@@ -1,16 +1,15 @@
 import socket
-udp_port=('172.16.5.254',2223)
+udp_port=('192.168.43.197',2225)
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.bind(udp_port)
 while True:
-	print "client:"
-	input=raw_input()
+	input=raw_input("server: ")
 	if input=="bye" or input=="exit":
-		s.sendto(input,('172.16.2.227',2223))
+		s.sendto(input,('192.168.43.13',2225))
 		s.close()
 	else:
-		s.sendto(input,('172.16.2.227',2223))
+		s.sendto(input,('192.168.43.13',2225))
 	#print "hi"
 	data,address=s.recvfrom(1024)
 	#print "hi"
-	print "server:"+str(data)
+	print "client: "+str(data)
